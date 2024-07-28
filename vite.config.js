973a9a67ddd0e92eeaaef93197526a110f0d0fd2
sben-ela <sbenarradia@gmail.com>
2024-07-28 12:45:41 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/E-commerce-website/',
   plugins: [react()],
   server :{
     // host : '10.13.1.8',
